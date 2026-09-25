ab4ec2d0fac8d62ee3b5a88aeafac2f40ef8d930
Pranav Hivarakar <coderheaven.blenderboy@gmail.com>
2026-09-25 23:20:15 +0530

    Bump version to 2.2.2 for corrected Google sign-in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.pranav.study.cet_study_sprint"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "2.2.1"
+        versionCode = 7
+        versionName = "2.2.2"
     }
 
     compileOptions {
